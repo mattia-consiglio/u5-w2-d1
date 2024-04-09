bb8375b9a6ba95a478b0dbb3552d5ab6da12b344
Mattia Consiglio <37270576+mattia-consiglio@users.noreply.github.com>
2024-04-09 15:47:33 +0200

    First commit

---
+++ src/test/java/mattiaconsiglio/u5w2d1/U5W2D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package mattiaconsiglio.u5w2d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W2D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
